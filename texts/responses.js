e8e767f09e295899b1441d08511951d9e3d396be
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-04-22 22:08:47 +0400

    first commit

---
+++ texts/responses.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+
+    "582567548:AAEQQdMSAJtwnTgn20V-56GxcGSBldyi8Nk": {
+        title: "",
+        sendInvitationText: (day, hour) => {
+            return `Отлично! Тогда мы с радостью ждём кандидатов по адресу: Екатеринбург, Советская 6А, ТЦ Мегамарт. Например во вторник на следующей неделе с 10.00 до 17.00 Тел. Для связи: 9122400263`
+        }
+    },
+
+    "588996159:AAGi9Eea2DMVzD3qGM3Ym-Y9tGYWh9t0TWw": {
+        title: "",
+        sendInvitationText: (day, hour) => {
+            return `связаться по телфону 89033211037`
+        }
+    }
+
+};
